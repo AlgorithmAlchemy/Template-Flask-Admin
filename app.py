ece2c751c35c5dd72cdc5435f657d32808d51708
AlgorithmAlchemy <darkydark@cyberfear.com>
2025-06-16 17:39:57 +0300

    Initial commit

--- app.py
+++ app.py
@@ -1,95 +1,30 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_admin import Admin, expose, AdminIndexView
+from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
 
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
-
+# Настройка приложения
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'mysecretkey'
-app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+# Выбери тему Bootswatch здесь:
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'  # например, cerulean, flatly, darkly и др.
 
 db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
 
-# Модель пользователя для базы + flask-login
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     email = db.Column(db.String(120))
-    password = db.Column(db.String(128))  # простой пароль
-    is_admin = db.Column(db.Boolean, default=False)
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-# Кастомный класс ModelView с авторизацией
-class AdminModelView(ModelView):
-    def is_accessible(self):
-        return current_user.is_authenticated and current_user.is_admin
-
-    def inaccessible_callback(self, name, **kwargs):
-        return redirect(url_for('login', next=request.url))
-
-# Кастомный класс для главной страницы админки с защитой
-class MyAdminIndexView(AdminIndexView):
-    def is_accessible(self):
-        return current_user.is_authenticated and current_user.is_admin
-
-    def inaccessible_callback(self, name, **kwargs):
-        return redirect(url_for('login', next=request.url))
-
-admin = Admin(app, name='My Admin Panel', index_view=MyAdminIndexView())
-admin.add_view(AdminModelView(User, db.session))
-
-# Создаем таблицы и администратора
 with app.app_context():
     db.create_all()
 
-    # Создаём admin заново
-    admin_user = User(
-        name='admin',
-        email='admin@example.com',
-        password='admin',  # сохраняем простой пароль
-        is_admin=True
-    )
-    admin_user.password = 'admin'  # хэшируем пароль через сеттер
-    db.session.add(admin_user)
-    db.session.commit()
-
-
-# Простой роут для логина
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        user = User.query.filter_by(email=email).first()
-        if user and user.check_password(password):
-            login_user(user)
-            next_page = request.args.get('next')
-            return redirect(next_page or url_for('admin.index'))
-        return 'Неверный логин или пароль'
-    return '''
-        <form method="post">
-            Email: <input type="text" name="email" /><br>
-            Пароль: <input type="password" name="password" /><br>
-            <input type="submit" value="Войти" />
-        </form>
-    '''
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
+# Инициализируем админку (без передачи template_mode или theme)
+admin = Admin(app, name='My Admin Panel')
+admin.add_view(ModelView(User, db.session, endpoint='user'))
 
 if __name__ == '__main__':
     app.run(debug=True)
-
